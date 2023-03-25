8d442eb65c67c1e7aea662ae7b1f5a181a7c81f6
Dion Tryban <github@diontryban.com>
2023-03-25 18:16:30 -0400

    More pushing mechanic implementation

--- Entities/Player/hamster.gd
+++ Entities/Player/hamster.gd
@@ -4,14 +4,14 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	
-	var anim_player = $AnimationPlayer
-	var animations = ['idle', 'walk','push','push_idle']
-	
-	
-	for animation in animations:
-		animation = anim_player.get_animation(animation)	
-		animation.loop_mode  = 1
+    
+    var anim_player = $AnimationPlayer
+    var animations = ['idle', 'walk','push','push_idle']
+    
+    
+    for animation in animations:
+        animation = anim_player.get_animation(animation)	
+        animation.loop_mode  = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
