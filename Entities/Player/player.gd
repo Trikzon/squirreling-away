7009cd7548c019e46ff7db51fb8e0828311c82a5
Alec Tremblay <tremblayalec14@gmail.com>
2023-03-25 09:02:18 -0400

    fixed player rotation and made platform larger
    idk why gitkraken is saying all of this code was changed when it's just a few lines, see discord for description of change

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -10,25 +10,25 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-    # Add the gravity.
-    if not is_on_floor():
-        velocity.y -= gravity * delta
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-    # Handle Jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var input_dir = Input.get_vector("player_left", "player_right", "player_forward", "player_backward")
-    var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
-    if direction:
-        velocity.x = direction.x * SPEED
-        velocity.z = direction.z * SPEED
-        
-        rotation.y = move_toward(rotation.y, atan2(direction.x, direction.z), ROTATION_SPEED)
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("player_left", "player_right", "player_forward", "player_backward")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		
+		look_at(Vector3.FORWARD.rotated(Vector3.UP, rotation.y).lerp( direction*-1 , 0.1) + position)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-    move_and_slide()
+	move_and_slide()
