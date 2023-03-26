dda8919c6aa339693e4e314112d4e67d2037d663
Alec Tremblay <tremblayalec14@gmail.com>
2023-03-26 00:02:17 -0400

    Auto stash before merge of "main" and "origin/main"

--- Entities/Player/hamster.gd
+++ Entities/Player/hamster.gd
@@ -14,5 +14,13 @@ func _ready():
         animation.loop_mode  = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+		if $AnimationTree.get("parameters/conditions/idle") and $boredom_timer.is_stopped():
+			$boredom_timer.start()
+			print("started")
 
+func _on_boredom_timer_timeout():
+	$AnimationTree.set("parameters/conditions/bored", true)
+	$AnimationTree.set("parameters/conditions/idle", false)
+	print("playing")
+	
