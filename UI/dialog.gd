3566cdab4930137f79858a562d30811bccb66a44
Brownbookcover <mike.ridgeway123@gmail.com>
2023-03-26 13:00:19 -0400

    Added Dialog Days

--- UI/dialog.gd
+++ UI/dialog.gd
@@ -1,15 +1,20 @@
 extends Control
 var root_node
+var TextArray
+var ArrSize
+var CurrentText
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     $NinePatchRect4.hide()
     play()
-    $NinePatchRect/BodyText.text = "Your mission today soldier is to hunt down the book we need to learn thier secrets"
     $NinePatchRect2/Speaker.text = "Fidel Squeaker"
     root_node=get_tree().root.get_child(0)
-    
-
+    var day=0                                   #THis should be passed in as a varaible
+    TextArray=dialog(day)                       #Pass this the day to load todays dialog
+    ArrSize=TextArray.size()                    #Sets up the data to read through array
+    $NinePatchRect/BodyText.text=TextArray[0]
+    CurrentText=1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,7 +23,12 @@ func _process(delta):
         $NinePatchRect4/ControlAnimation.play("Wiggle")
         
     if Input.is_action_just_pressed("dialog_forward"):
-        hide()
+        if CurrentText==ArrSize:                #If at last element hide the box
+            hide()
+        else:                                   #If not iterate the current text and replay the animation
+            CurrentText+=1
+            play()
+            $NinePatchRect/BodyText.text=TextArray[CurrentText-1]
 
 func play():
     $NinePatchRect/BodyText/TextAnimation.play("Text Animation")
@@ -26,3 +36,10 @@ func play():
 
 func _on_text_animation_animation_finished(anim_name):
     $NinePatchRect4.show()
+
+func dialog(day):
+    var array
+    if day==0:
+        array=["First dialog", "Second dialog", "Third dialog"]
+    return array
+        
